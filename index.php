0b920069110590c19bca959ff224f50f86589aa9
christian215 <cristian.medina.hur@cali.gov.co>
2018-11-14 19:40:58 -0500

    actualizacion de archivos

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Prueba de Heroku</title>
+</head>
+<body>
+	<header>
+		<h1>esto es prueba heroku</h1>
+	</header>
+	<section>
+		<div>esto es un mensaje de prueba para heroku</div>
+	</section>
+	<footer>
+		<ul>
+			<li>direccion:</li>
+			<li>telefono:</li>
+			<li>email:</li>
+		</ul>
+	</footer>
+
+</body>
+</html>
